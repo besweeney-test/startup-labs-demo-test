0bbf16f0eb7fd954a5e1825021abcf3a99cd4495
besweeney <besweeney@google.com>
2024-07-24 20:43:18 -0700

    change website text for gh actions

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React or dont i dont care
         </a>
       </header>
     </div>
